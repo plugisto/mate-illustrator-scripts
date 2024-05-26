008988931b6f1e980d27e98ae7df1f38ae7e9556
Eugen Pflueger <eugen.pflueger@plugisto.com>
2024-05-26 22:29:22 +0200

    added comments

--- MATE_tip_01_Random_Opacity_for_Selected_Items.jsx
+++ MATE_tip_01_Random_Opacity_for_Selected_Items.jsx
@@ -1,6 +1,12 @@
+// Random opacity for selected items
+// Created with MATE, www.omata.io/mate
+// v1.0
 
 var doc = app.activeDocument;
 var selectedItems = doc.selection;
+
+var min = 20;
+var max = 80;
 
 if (selectedItems.length === 0) {
     alert("No items selected.");
@@ -8,7 +14,7 @@ if (selectedItems.length === 0) {
     for (var i = 0; i < selectedItems.length; i++) {
         var item = selectedItems[i];
         if (!item.locked && !item.hidden) {
-            var randomOpacity = Math.random() * (80 - 20) + 20;
+            var randomOpacity = Math.random() * (max - min) + min;
             item.opacity = randomOpacity;
         }
     }
